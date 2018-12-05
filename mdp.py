4adb3143bfa6ebdc0baeefac9e5102b0281246b1
Ammar Alqatari <ammar.qat@gmail.com>
2018-12-04 23:08:23 -0800

    most of mdp

--- mdp.py
+++ mdp.py
@@ -3,8 +3,9 @@
 
 import math, random, itertools
 from collections import defaultdict
+from deuces import Deck, Evaluator
 
-class EpidemicMDP:
+class PokerMDP:
 
 	#################################################################################
 	#								GET ACTIONS 									#
@@ -12,6 +13,12 @@ class EpidemicMDP:
 
 	# Given a  state, returns all possible actions. 
 	def getActions(self, state):
+		playerHand, playerBet = state.players[state.curPlayer]
+		callVal = state.curBet - playerVet
+		RAISE = range(call, call + self.maxRaise, 10)
+		RAISE.append(-1)
+
+		return RAISE
 
 
 	#################################################################################
@@ -20,10 +27,28 @@ class EpidemicMDP:
 
 	# Checks to see if a state is a terminal state. 
 	def isEnd(self, state):
-		
+		folded = sum([1 if not player else 0 for player in state.players])
+		return len(state.board) == 5 or folded == self.numPlayers - 1
+
+	def getWinner(self, state):
+		results = [Evaluator.evaluate(player[0]) for player in state.curPlayer]
+
+		return results.index(max(results))
 
 	# Return reward for a given state
-	def getReward(self, state): 
+	def getReward(self, state):
+		playerHand, playerBet = state.players[state.curPlayer]
+
+		if not playerHand:
+			return 0
+		if isEnd(state):
+			winner = getWinner(state)
+			if state.curPlayer == winner:
+				return state.pot
+			else:
+				return 0
+		else:
+			return -1*playerBet
 
 
 	# Generates a next state probabilistically based on current state. 
@@ -35,10 +60,20 @@ class EpidemicMDP:
 
 	# builds initial state 
 	def initState(self):
+		state = {}
+		state['board'] = []
+		state['pot'] = 0
+		state['curBet'] = 0
+		state['curPlayer'] = 0
+		#player = (hand, curBet), hand=False if player has folded
+		state['players'] = [(self.deck.draw(2), 0) for _ in range(self.numPlayers)]
 
 		return state
 
 
 	def __init__(self):
+		self.numPlayers = 4
+		self.deck = Deck()
+		self.maxRaise = 51
 		#DECLARE GLOBAL VARIABLES (HYPERPARAMETERS) HERE!
 		self.state = self.initState()
